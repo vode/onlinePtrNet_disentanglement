950eab59bbd7ee08b3bca0cd31cd4bc3b4c56acf
vode <kylayamata@hotmail.com>
2021-05-23 21:09:26 +0800

    revise eval.sh

--- eval.sh
+++ eval.sh
@@ -1,3 +1,4 @@
-wget https://www.dropbox.com/s/ir7sweersamq834/processed_data.pkl?dl=0 ./data
+wget https://www.dropbox.com/s/ir7sweersamq834/processed_data.pkl
+mv processed_data.pkl ./data
 python3 disentangle.py esim4 --model ./data/esim_joint.pt.model --test ./data/test/*annotation.txt --hidden 128  --test-start 1000 --test-end 2000  --word-vectors glove-ubuntu.txt > cluster.out
 python3 task-4-evaluation.py --gold ./data/test/*anno* --auto cluster.out
